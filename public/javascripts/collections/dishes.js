99a97b62d42bb9dac8e2ef34bdd617a81554b20f
theotrain <owen@localhost-2.local>
2017-06-21 09:19:25 -0500

    clarify getItem and hasItem

--- public/javascripts/collections/dishes.js
+++ public/javascripts/collections/dishes.js
@@ -1,6 +1,6 @@
 var DishesCollection = Backbone.Collection.extend({
   model: DishModel,
   hasItem: function(id) {
-    return !!this.findWhere({id: id});
+    return !!this.get(id);
   }
 });
--- public/javascripts/views/item_panel.js
+++ public/javascripts/views/item_panel.js
@@ -2,7 +2,6 @@ var ItemPanelView = Backbone.View.extend({
   el: $('#item_details')[0],
   template: App.templates.item_panel,
   render: function() {
-    console.log('we in render');
     this.$el.append(this.template(this.model));
     return this;
   },
